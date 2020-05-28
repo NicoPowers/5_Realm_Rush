0de2505c439efc97db9436c5de6d027f407209a4
Nick <nicolasmontoya@yahoo.com>
2020-05-28 16:35:50 -0400

    first commit

---
+++ Assets/Scripts/EditorSnap.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour
+{
+    [Range(1f, 20f)][SerializeField] float snapDistance = 10f;
+
+
+
+    private void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+
+        Vector3 snapPos;
+        snapPos.x = Mathf.RoundToInt(transform.position.x / snapDistance) * snapDistance;
+        snapPos.y = 0f;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / snapDistance) * snapDistance;
+
+        transform.position = snapPos;
+
+
+
+    }
+}
